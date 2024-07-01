8c8b769be0d491c98204b61db359f447c7b06404
ViktorGolikoff <pravgolvik@gmail.com>
2024-07-01 19:46:01 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# 1st program
+print((9 ** 0.5) * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print((1234 % 1000) // 10 + (5678 % 1000) // 10)
+
+# 4th program
+a = 13.42
+b = 42.13
+
+a1 = int(a)
+b1 = int(b)
+
+a2 = int(a * 100) % 1300
+b2 = int(b * 100) % 4200
+
+print(a1==b2 or a2==b1)
+
